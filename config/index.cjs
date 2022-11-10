472863e4981142868b89091f758667071340cffa
ljcoi <117892035+ljcoi@users.noreply.github.com>
2022-11-10 15:33:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'ofBdO5rgSEsrX8UtOAynFSXd9bCA',
+  CALLBACK_TEMPLATE_ID: '3hb18BZ4z4jNpiPBT85VqCQoMzEyxtWiaZ9bRvGZdf8',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'ofBdO5v_9AwbBIvx0XSfLmf7TYys','ofBdO5rgSEsrX8UtOAynFSXd9bCA'
     }
   ],
 
